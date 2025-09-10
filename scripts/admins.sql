cb4011d8dabb85cb65487a5998a9a94c84f7bab4
ksoliman05 <kerloussoliman05@gmail.com>
2025-09-10 10:43:39 -0400

    Created admins.sql

---
+++ scripts/admins.sql
@@ -0,0 +1,16 @@
+SHOW DATABASES;
+
+CREATE TABLE admins (
+adminID      INT(11)      NOT NULL   AUTO_INCREMENT,
+emailAddress VARCHAR(255) NOT NULL   UNIQUE,
+password     CHAR(64)     NOT NULL,
+firstName    VARCHAR(60)  NOT NULL,
+lastName     VARCHAR(60)  NOT NULL,
+PRIMARY KEY (adminID)
+);
+
+SHOW TABLES;
+
+SHOW CREATE TABLE admins;
+
+DESCRIBE admins;
